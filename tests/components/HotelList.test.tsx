5a24746abde757ce9e382cadad4e787b9931f663
Jason Holloway <miceeatnicerice@hotmail.com>
2018-11-07 19:21:07 +0000

    testing basic initial rendering

--- tests/components/HotelList.test.tsx
+++ tests/components/HotelList.test.tsx
@@ -1,14 +1,40 @@
-import { mount } from "enzyme";
+import { mount, ReactWrapper } from "enzyme";
 import React, { Component } from "react";
 import "jest-enzyme"
 
 class HotelList extends Component {
     render() {
-        return <h1>Hello!</h1>
+        return <section id="hotelList">
+                    Hello!
+                    <ul>
+
+                    </ul>
+                </section>
     }
 }
 
-test('HotelList renders', () => {
-    const dom = mount(<HotelList/>);
-    expect(dom.find('h1')).toExist();
+describe('HotelList', () => {
+
+    let dom: ReactWrapper;
+    let section: ReactWrapper;
+
+    beforeEach(() => {
+        dom = mount(<HotelList/>);
+        section = dom.find('section#hotelList')
+    })
+
+    it('renders as <section> with id="hotelList"', () => {
+        expect(section).toExist();
+    })
+
+    it('contains single <ul>', () => {
+        expect(section).toContainExactlyOneMatchingElement('ul');
+    })
+
+    describe('at first', () => {
+        it('lists no hotels', () => {
+            expect(section.find('ul li')).not.toExist();
+        })
+    })
+
 })
